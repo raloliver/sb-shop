1cee54988675af29bda94949c4cc12e8b1276263
raloliver <contato@raloliver.com.br>
2021-07-22 15:47:02 +0100

    Add files to project

---
+++ src/main/java/br/com/raloliver/sbshop/SbShopApplication.java
@@ -0,0 +1,13 @@
+package br.com.raloliver.sbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbShopApplication.class, args);
+	}
+
+}
